ce6da3051b8e9a272b6c93fa1afe1f1e297c9d9e
Evan Moon <bboydart91@gmail.com>
2021-07-08 15:47:02 +0900

    feat: localstorage 유틸 추가 (#6)
    * feat: localstorage 유틸 추가
    
    * export modules
    
    * feat: add popLocalStorageItem
    
    * getLocalStorageItem 예외처리

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { default as logger, FirebaseConfig } from './logger';
 export { default as delay } from './delay';
+export {
+  setLocalStorageItem,
+  getLocalStorageItem,
+  removeLocalStorageItem,
+  clearLocalStorage,
+} from './localStorage';
