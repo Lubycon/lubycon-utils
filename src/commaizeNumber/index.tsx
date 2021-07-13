ad22ad3e2a619da1e7a9a30659fd141193c9dc49
evan-moon <bboydart91@gmail.com>
2021-07-13 23:40:42 +0900

    feat: commaizeNumber 함수 추가

---
+++ src/commaizeNumber/index.tsx
@@ -0,0 +1,12 @@
+const commaizeByRegex = (value: string) => value.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+
+/**
+ * 인자로 주어진숫자의 3자리수 마다 콤마를 찍은 문자열을 반환합니다. ex)  1000000 -> 1,000,000
+ */
+export function commaizeNumber(value: number | string) {
+  if (typeof value === 'number' && value.toLocaleString != null) {
+    return value.toLocaleString();
+  }
+
+  return commaizeByRegex(String(value));
+}
