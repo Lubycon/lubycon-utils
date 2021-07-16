2365482fe20a10fd9453aaa629c86123702a23a8
geonwoo.jeong <doscmdev@gmail.com>
2021-07-16 17:41:59 +0900

    style: apply prettier

--- .releaserc.js
+++ .releaserc.js
@@ -1,31 +1,34 @@
- module.exports = {
+module.exports = {
   branches: [
     {
-      "name": "main"
+      name: 'main',
     },
   ],
   plugins: [
-    ["@semantic-release/commit-analyzer", {
-      "preset": "angular",
-      "releaseRules": [
-        {"type": "feat", "release": "minor"},
-        {"type": "fix", "release": "patch"},
-        {"type": "chore", "release": "patch"},
-        {"type": "refactor", "release": "patch"},
-        {"type": "style", "release": "patch"}
-      ],
-      "parserOpts": {
-        "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES"]
-      }
-    }],
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
     [
-      "@semantic-release/git",
+      '@semantic-release/commit-analyzer',
       {
-        "assets": ["package.json"]
-      }
+        preset: 'angular',
+        releaseRules: [
+          { type: 'feat', release: 'minor' },
+          { type: 'fix', release: 'patch' },
+          { type: 'chore', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'style', release: 'patch' },
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+        },
+      },
     ],
-    "@semantic-release/github"
-  ]
-}
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'],
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
